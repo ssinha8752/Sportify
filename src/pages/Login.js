6fca13f7f1435274716ad0d2d7e91d0e601ee80c
ssinha8752 <shubhamsinha8752@yahoo.com>
2024-04-30 03:03:23 +0530

    [Shubham][29-Apr] Spotify - Guest Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -24,6 +24,11 @@ export const Login = () => {
       }
     }
 
+    function handleLoginAsGuest(){
+      email.current.value="guest@gmail.com"
+      password.current.value="12345678"
+    }
+
 
     return (
       <main>
@@ -41,7 +46,7 @@ export const Login = () => {
             </div>
             <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
           </form>
-          {/* <button className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> */}
+          <button onClick={handleLoginAsGuest} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button>
       </main>
     )
   }
